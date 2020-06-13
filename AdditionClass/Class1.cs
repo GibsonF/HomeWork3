309b7e8651cc4767a5ac93dcfd4512c427edc4cc
Владислав Дойнов <gibsonferman@yandex.ru>
2020-06-13 09:32:02 +0300

    Есть первое и третье задание, вроде все в порядке.

--- AdditionClass/Class1.cs
+++ AdditionClass/Class1.cs
@@ -9,4 +9,166 @@ namespace AdditionClass
     public class MyClass
     {
     }
+
+    public class Complex
+    {
+        private double im;
+        private double re;
+
+        public Complex()
+        {
+            im = 0;
+            re = 0;
+        }
+
+        public Complex(double re, double im)
+        {            
+            this.im = im;
+            this.re = re;
+        }
+        public Complex Plus(Complex x2)
+        {
+            Complex x3 = new Complex();
+            x3.im = x2.im + im;
+            x3.re = x2.re + re;
+            return x3;
+        }
+        public Complex Minus(Complex x)
+        {
+            Complex NewX = new Complex();
+            NewX.im = im - x.im;
+            NewX.re = re - x.re;
+            return NewX;
+        }
+        public Complex Multi(Complex x)
+        {
+            Complex NewX = new Complex();
+            NewX.re = (x.re * this.re)-(x.im*this.im);
+            NewX.im = this.re * x.im + x.re * this.im;
+            return NewX;
+        }
+        public double Im
+        {
+            get { return im; }
+            set
+            {
+               im = value;
+            }
+        }
+        public double Re
+        {
+            get { return re; }
+            set
+            {
+                re = value;
+            }
+        }
+        // Специальный метод, который возвращает строковое представление данных.
+        public string Tostring()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+    public class Fraction
+    {
+        private int numinator;
+        private int denominator;
+        public Fraction()
+        {
+            numinator = 1;
+            denominator = 1;
+        }
+        public Fraction(int numinator,int denominator)
+        {
+            
+                try
+                {
+                    if (denominator == 0) throw new ArgumentException();
+                    this.numinator = numinator;
+                    this.denominator = denominator;
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+        }
+
+        public int Numinator
+        {
+            get { return numinator; }
+            set { numinator = value; }
+        }
+
+        public int Denominator
+        {
+            get { return denominator; }
+            set { denominator = value; }
+        }
+
+        public double DecFrac
+        {
+            get
+            {
+                return numinator / denominator;
+            }
+        }
+
+        public Fraction Plus(Fraction x)
+        {
+            Fraction y = new Fraction();
+            y.Denominator = this.denominator * x.Denominator;
+            y.Numinator = this.denominator * x.Numinator + this.numinator * x.Denominator;
+            y = Simplify(y);
+            return y;
+        }
+
+        public Fraction Minus(Fraction x)
+        {
+            Fraction y = new Fraction();
+            y.Denominator = this.denominator * x.Denominator;
+            y.Numinator =   this.numinator * x.Denominator- this.denominator * x.Numinator;
+            y = Simplify(y);
+            return y;
+        }
+
+        public Fraction Multi(Fraction x)
+        {
+            return Simplify(new Fraction(this.numinator * x.Numinator, this.denominator * x.Denominator));
+        }
+
+        public Fraction Div(Fraction x)
+        {
+            return Simplify(new Fraction(this.numinator * x.Denominator, this.denominator * x.Numinator));
+        }
+
+        public static Fraction Simplify(Fraction x)
+        {
+            Fraction y = new Fraction(x.Numinator, x.Denominator);
+            int max,maxdiv=-1;
+            max= (y.Numinator > y.Denominator ? max = y.Numinator : max = y.Denominator);
+            while (true){
+                maxdiv = -1;
+                for (int i = 1; i <= max; i++)
+                {
+                    if(y.Numinator % i==0&&y.Denominator % i==0)
+                    {
+                        if (i >= maxdiv) maxdiv = i; 
+                    }
+                }
+                if (maxdiv == 1) break;
+                else
+                {
+                    y.Numinator /= maxdiv;
+                    y.Denominator /= maxdiv;
+                }
+                max = (y.Numinator > y.Denominator ? max = y.Numinator : max = y.Denominator);
+
+            }
+            return y;
+        }
+        public string Tostring()
+        {
+            return numinator + "/" + denominator;
+        }
+    }
 }
--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -9,8 +9,140 @@ namespace HomeWork3
 {
     class Program
     {
-        static void Main(string[] args)
+         
+
+    struct Complexs
+    {
+        public double im;
+        public double re;
+           
+        public Complexs Plus(Complexs x)
         {
+            Complexs y;
+            y.im = im + x.im;
+            y.re = re + x.re;
+            return y;
         }
+        public Complexs Minus(Complexs x)
+        {
+                Complexs NewY;
+                NewY.im = im - x.im;
+                NewY.re = re - x.re;
+                return NewY;
+        }
+        public Complexs Multi(Complexs x)
+        {
+            Complexs y;
+            y.im = re * x.im + im * x.re;
+            y.re = re * x.re - im * x.im;
+            return y;
+        }
+        public string Tostring()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+
+        public static void FirstTask()
+        {
+            Complexs xs, ys;
+            xs.im = 4;
+            xs.re = 3;
+            ys.re = 4;
+            ys.im = 3;
+            Complex x = new Complex(3, 4);
+            Complex y = new Complex(4, 3);
+            Console.WriteLine("Представление комплЕксных чисел: re+ i*im");
+            Console.WriteLine("пункт А, задание 1 (структура Complex)");
+            Console.WriteLine("Есть два комплексных числа: x=3+4i и y=4+3i.\nПроведем операции: x-y. Должно получиться -1 + 1i.");
+            Console.WriteLine(xs.Minus(ys).Tostring());
+            Console.WriteLine("пункт Б, задание 1 (класс Complex)");
+            Console.WriteLine("Есть два комплексных числа: x=3+4i и y=4+3i.\nПроведем операции: x*y+x-y. Должно получиться -1 + 26i.");
+            Console.WriteLine(x.Multi(y).Plus(x).Minus(y).Tostring());
+            Console.WriteLine("пункт В, задание 1 (класс Complex+switch)");
+            string i = "1";
+            Complex z = new Complex();
+            Complex buf;
+            while (i != "0")
+            {
+                Console.WriteLine("1-задать начальное число(сначала re, потом im)\n2-прибавить к текущему числу новое(ввести новое число)\n3-вычесть из текущего числа новое(ввести новое число)\n4-умножить текущее число н новое(ввести новое число)\n0-выйти");
+                i = Console.ReadLine();
+                switch (i)
+                {
+                    case "1":
+                        z = new Complex(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+                        break;
+                    case "2":
+                        buf = new Complex(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+                        z = z.Plus(buf);
+                        break;
+                    case "3":
+                        buf = new Complex(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+                        z = z.Minus(buf);
+                        break;
+                    case "4":
+                        buf = new Complex(Convert.ToDouble(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+                        z = z.Multi(buf);
+                        break;
+                    case "0":
+                        Console.WriteLine("Финальный результат: " + z.Tostring());
+                        break;
+                }
+                if (i != "0") Console.WriteLine("Результат: " + z.Tostring());
+            }
+            Console.ReadKey();
+        }
+        public static void ThirdTask()
+        {
+            string i = "1";
+            Fraction f=new Fraction();
+            while (i != "0")
+            {
+                Console.WriteLine("1-задать начальное число(сначала числитель, потом делитель)\n2-прибавить к текущему числу новое(ввести новое число)\n3-вычесть из текущего числа новое(ввести новое число)\n4-умножить текущее число н новое(ввести новое число)\n5-упростить дробь(введите новую)\n0-выйти");
+                i = Console.ReadLine();
+                switch (i)
+                {
+                    case "1":
+                        f = new Fraction(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+                        f=Fraction.Simplify(f);
+                        break;
+                    case "2":
+                        f = f.Plus(new Fraction(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+                        break;
+                    case "3":
+                        f = f.Minus(new Fraction(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+                        break;
+                    case "4":
+                        f = f.Multi(new Fraction(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+                        break;
+                    case "5":
+                        Fraction.Simplify(f).Tostring();
+                        break;
+                    case "0":
+                        Console.WriteLine("Финальный результат: " + f.Tostring());
+                        break;
+                }
+                if (i != "0") Console.WriteLine("Результат: " + f.Tostring());
+            }
+            Console.ReadKey();
+        }
+    static void Main(string[] args)
+        {
+            string i = "1";
+            while (i != "0")
+            {
+                Console.WriteLine("1-первое задание. 3-третье задание");
+                 i = Console.ReadLine();
+                switch (i)
+                {
+                    case "1": FirstTask(); break;
+                    case "3": ThirdTask(); break;
+                }
+            }
+               
+            Console.ReadKey();
+        }
+
+
     }
 }
